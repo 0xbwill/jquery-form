95b6b4e7c6416e917a212988853c1df3d44f5771
0xbwill <william.degh@gmail.com>
2022-06-28 16:20:58 +0200

    fix
    • default select option disabled
    • redirect to form if mail.php is reached by URL, without submiting form

--- mail.php
+++ mail.php
@@ -1,33 +1,30 @@
 <?php
-if(!isset($_POST['submit']))
-{
-// Veuillez 
-	echo "L'envoi du formulaire a échoué, veuillez réessayer.";
+if (!isset($_POST)) {
+    header("location:index.html");
+} else {
+    $entreprise = $_POST['entreprise'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $name = $_POST['tel'];
+    $mail = $_POST['mail'];
+    $tel = $_POST['mail'];
+    $demande = $_POST['demande'];
+    $besoin = $_POST['besoin'];
+
+    // Informations du mail
+    $email_from = 'willdgh.pro@gmail.com';
+    $email_subject = $demande;
+    $email_body = $nom . " " . $prenom . " " . "vous a envoyé un message\n" .
+        "La demande est la suivante:\n $besoin\n" . "Téléphone : " . $tel . "\n" . "Mail : " . $mail;
+
+    // Destinataire du mail
+    $to = "cyprien.prouvot@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    // Envoi du mail
+    mail($to, $email_subject, $email_body, $headers);
+
+    // Envoi effectué, redirection vers la page email-sent.php
+    header('Location: email-sent.html');
 }
-
-// Récupération des variables du formulaire
-$entreprise = $_POST['entreprise'];
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$name = $_POST['tel'];
-$mail = $_POST['mail'];
-$tel = $_POST['mail'];
-$demande = $_POST['demande'];
-$besoin = $_POST['besoin'];
-
-// Informations du mail
-$email_from = 'willdgh.pro@gmail.com';
-$email_subject = $demande;
-$email_body = "Vous avez reçu un message de la part de $nom\n".
-    "La demande est la suivante:\n $besoin".
-    
-// Destinataire du mail
-$to = "william.degh@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-
-// Envoi du mail
-mail($to,$email_subject,$email_body,$headers);
-
-// Envoi effectué, redirection vers la page email-sent.php
-header('Location: email-sent.html');
